9c21cbbde39b3ec220090a13c1d0143929d62c48
Mickey Burks <burks023@gmail.com>
2014-04-01 20:37:53 -0700

    Forgot sharps

--- peasants.h
+++ peasants.h
@@ -1,4 +1,10 @@
 
+#ifndef M4MM_PEASANTS_H
+#define M4MM_PEASANTS_H
+
+#include "vector3.h"
+#include "vector4.h"
+
 // Look at
 Vector3<Value> stare(const Vector<Value> &eye, const Vector<Value> &center, const Vector<Value> &up);
 // Orbit
@@ -7,3 +13,7 @@ Vector3<Value> orbit(const Vector<Value> &position, const Vector<Value> &center,
 Vector3<Value> pan(const Vector<Value> &vector, const Vector<Value> &direction);
 // Rotate
 Vector3<Value> rotate(const Vector<Value> &position, const Vector<Value> &axis);
+
+
+
+#endif // M4MM_PEASANTS_H
